81ed7f9e5d9fd9e64d531d4bf4215b99f91a8ad2
RafaPatino01 <RafaPatino01@outlook.com>
2022-04-27 10:24:18 -0500

    fixed backend

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-fetch('http://146.190.1.241/token_names')
+let mode = 0 // Mode 0 es desarrollo local
+let server = ""
+
+if(mode == 0){
+  server = "http://localhost:3000"
+}
+else {
+  server = "http://146.190.1.241"
+}
+
+fetch(server + '/get_users')
   .then(response => response.json())
   .then(data => console.log(data));
