4ffdce4abdb83a402350fc9b93497044bd0afc78
RafaPatino01 <RafaPatino01@outlook.com>
2022-04-28 14:09:55 -0500

    Aplha 1.0.1 patched

--- js/login.js
+++ js/login.js
@@ -1,13 +1,3 @@
-async function hash(string) {
-    const utf8 = new TextEncoder().encode(string);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
-      .map((bytes) => bytes.toString(16).padStart(2, '0'))
-      .join('');
-    return hashHex;
-}
-
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
@@ -25,11 +15,9 @@ function register(){
     const mail = document.getElementById("email")
 
     if(user.value != "" && pass.value != "" && mail.value != ""){
-        hash(pass.value).then((hex) => { 
-            fetch(server + '/register/' + mail.value + '/' + user.value + "/" + hex)
-            // Wait 1 sec and load login
-            delay(1000).then(() => window.location.href = "login.html")
-        });
+        fetch(server + '/register/' + mail.value + '/' + user.value + "/" + sha256(pass.value))
+        // Wait 1 sec and load login
+        delay(1000).then(() => window.location.href = "login.html")
     }
     else {
         alert("Faltan datos")
@@ -41,21 +29,19 @@ function login(){
     const pass = document.getElementById("pass")
 
     if(user.value != "" && pass.value != ""){
-        hash(pass.value).then((hex) => { 
-            fetch(server + '/login/' + user.value + '/' + hex)
-             .then(response => response.json())
-             .then(data => {
-                if(data[0] == "OK"){
-                    console.log("Login Correcto")
-                    setCookie("session_metastonks",user.value,1)
-                    delay(500).then(() => window.location.href = "index.html")
-                }
-                else {
-                    alert("Login Incorrecto")
-                }
-                
-            }); 
-        })
+        fetch(server + '/login/' + user.value + '/' + sha256(pass.value))
+            .then(response => response.json())
+            .then(data => {
+            if(data[0] == "OK"){
+                console.log("Login Correcto")
+                setCookie("session_metastonks",user.value,1)
+                delay(500).then(() => window.location.href = "index.html")
+            }
+            else {
+                alert("Login Incorrecto")
+            }
+            
+        }); 
     }
     else {
         alert("Faltan datos")
